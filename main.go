0ce248fa4b8dc7f96997d31bfea83d5230ef6554
Nikita Skulkin <clonerpresent@gmail.com>
2024-08-27 19:49:13 +0300

    add /help functionality

--- main.go
+++ main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"log"
-    "github.com/sklknn/gotelegramm/pkg/commands"
-    "time"
+	"os"
+	"time"
+
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
-    "os"
+	//"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/conversation"
+	"github.com/sklknn/gotelegramm/pkg/commands"
 )
 
 func main() {
 
-    token, err := os.ReadFile("./TOKEN.txt")
-    if err != nil {
-        log.Println("Не удалось получить токен: " + err.Error())
-    }
+	token, err := os.ReadFile("./TOKEN.txt")
+	if err != nil {
+		log.Println("Не удалось получить токен: " + err.Error())
+	}
 
-    println(string(token))
+	log.Println("Токен успешно получен")
 
-    bot, err := gotgbot.NewBot(string(token)[:len(token)-1], nil)
+	bot, err := gotgbot.NewBot(string(token)[:len(token)-1], nil)
 	if err != nil {
 		panic("Не удалось создать нового бота: " + err.Error())
 	}
-    
+
 	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
 		// if error is returned by a handler - continue
 		Error: func(bot *gotgbot.Bot, context *ext.Context, err error) ext.DispatcherAction {
@@ -35,6 +37,22 @@ func main() {
 	updater := ext.NewUpdater(dispatcher, nil)
 	//introduce
 	dispatcher.AddHandler(handlers.NewCommand("start", commands.Start))
+    //user help
+    dispatcher.AddHandler(handlers.NewCommand("help", commands.Help))
+    //conversation 
+//    dispatcher.AddHandler(handlers.NewConversation(
+//        []ext.Handler{handlers.NewCommand("pizza", commands.Conversation)},
+//        map[string][]ext.Handler{
+//            NAME : {handlers.NewMessage(noCommands, name)},
+//            PIZZA : {handlers.NewMessage(noCommands, pizza)},
+//            ADDRESS : {handlers.NewMessage(noCommands, address)},
+//        },
+//        &handlers.ConversationOpts{
+//            Exits : []ext.Handler{handlers.NewCommand("cancel_order", commands.EndConversation)},
+//            StateStorage : conversation.NewInMemoryStorage(conversation.KeyStrategySenderAndChat),
+//    AllowReEntry : true,
+//        },
+//        ))
 
 	err = updater.StartPolling(bot, &ext.PollingOpts{
 		DropPendingUpdates: true,
@@ -52,4 +70,3 @@ func main() {
 
 	updater.Idle()
 }
-
--- pkg/commands/commands.go
+++ pkg/commands/commands.go
@@ -1,9 +1,9 @@
 package commands
 
 import (
-    "github.com/PaulSonOfLars/gotgbot/v2"
+	"fmt"
+	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-    "fmt"
 )
 
 func Start(bot *gotgbot.Bot, context *ext.Context) error {
@@ -12,6 +12,16 @@ func Start(bot *gotgbot.Bot, context *ext.Context) error {
 	})
 	if err != nil {
 		return fmt.Errorf("Не удалось отправить сообщение start: %w", err)
+	}
+	return nil
+}
+
+func Help(bot *gotgbot.Bot, context *ext.Context) error {
+	_, err := context.EffectiveMessage.Reply(bot, fmt.Sprintf("Я умею заказывать пиццу /order, веселить людей, а еще хвалить моего создателя! Он превосходный начинающий программист и системый администратор"), &gotgbot.SendMessageOpts{
+		ParseMode: "HTML",
+	})
+	if err != nil {
+		return fmt.Errorf("Не удалось отправить сообщение help: %w", err)
 	}
 	return nil
 }
